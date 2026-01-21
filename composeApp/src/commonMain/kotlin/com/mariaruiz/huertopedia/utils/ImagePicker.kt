1e8f1ca713cd6ae0dd99eca0af015b9acce580fb
María <119685476+mariaruizpaton@users.noreply.github.com>
2026-01-21 09:45:15 +0100

    Error clase Data arreglado

--- composeApp/src/commonMain/kotlin/com/mariaruiz/huertopedia/utils/ImagePicker.kt
+++ composeApp/src/commonMain/kotlin/com/mariaruiz/huertopedia/utils/ImagePicker.kt
@@ -1,6 +1,7 @@
 package com.mariaruiz.huertopedia.utils
 
 import androidx.compose.runtime.Composable
+import dev.gitlive.firebase.storage.Data // Importante añadir este import
 
 @Composable
 expect fun rememberImagePicker(onImagePicked: (ByteArray?) -> Unit): ImagePickerLauncher
@@ -8,3 +9,7 @@ expect fun rememberImagePicker(onImagePicked: (ByteArray?) -> Unit): ImagePicker
 expect class ImagePickerLauncher {
     fun launch()
 }
+
+// Añadimos esta línea: es la promesa de que cada plataforma
+// sabrá convertir los bytes al formato de Firebase
+expect fun ByteArray.toFirebaseData(): Data
